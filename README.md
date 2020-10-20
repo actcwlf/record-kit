8e9f2e0fb0b4680dc4464570d4257c5bdcc3104c
actcwlf <actcwlf@qq.com>
2020-10-20 15:29:10 +0800

    add default records path to Recorder

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # record-kit
 A simple record toolkit for experiment.
+
+##  Installation
+```bash
+pip install record-kit
+```
+
+## Usage
--- record_kit/recorder.py
+++ record_kit/recorder.py
@@ -1,29 +1,54 @@
-class Recorder:
-    def __init__(self, file_name):
-        self.file_name = file_name
-        self._write_line("# Experiment Record")
+import pathlib
+import time
+import os
 
-    def _write(self, s):
-        with open(self.file_name, "a") as f:
+
+def check_suffix(file_name):
+    suffixes = file_name.suffixes
+    if len(suffixes) < 1 or suffixes[-1] != '.md':
+        suffixes.append('.md')
+        suffix = ''.join(suffixes)
+        file_name.with_suffix(suffix)
+    return str(file_name.absolute())
+
+
+class LogBase:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def append(self, s):
+        with self.file_path.open("a") as f:
             f.write(s)
 
-    def _write_line(self, s):
-        with open(self.file_name, "a") as f:
+    def append_line(self, s):
+        with self.file_path.open("a") as f:
             f.write(s + "\n")
+
+
+class Recorder(LogBase):
+    def __init__(self, file_name='record', records_dir='./records', with_timestamp=True):
+        self.records_dir = pathlib.Path(records_dir)
+        os.mkdir(self.records_dir)
+        if with_timestamp:
+            timestamp = time.strftime('%Y%m%d-%H%M%S', time.localtime(time.time()))
+            file_name += '-' + timestamp
+        self.record_name = self.records_dir.joinpath(file_name + '.md')
+        super().__init__(self.record_name)
+        self.append_line(f"# {file_name}")
 
     def begin_meta(self):
         s = "## Meta\n" + \
             "| key | value |\n" + \
             "| :-: |  :-:  |\n"
-        self._write(s)
+        self.append(s)
 
     def begin_data(self):
         s = "## Data"
-        self._write_line(s)
+        self.append_line(s)
 
     def write_meta(self, key, value):
         s = f"| {key} | {str(value)} |"
-        self._write_line(s)
+        self.append_line(s)
 
     def write_header(self, *args):
         length = len(args)
@@ -31,11 +56,11 @@ class Recorder:
         s = " | ".join(args)
         s = "| " + s + " |"
         s2 = "| :-: " * length + "|"
-        self._write_line(s)
-        self._write_line(s2)
+        self.append_line(s)
+        self.append_line(s2)
 
     def write_data(self, *args):
         args = map(str, args)
         s = " | ".join(args)
         s = "| " + s + " |"
-        self._write_line(s)
+        self.append_line(s)

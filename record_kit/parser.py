0d79ec58ca566e0e655fcaf96bc2383033d2e43c
actcwlf <siriustony@163.com>
2022-07-12 12:13:46 +0800

    parsing data section by pd.read_csv

--- record_kit/parser.py
+++ record_kit/parser.py
@@ -1,3 +1,5 @@
+from io import StringIO
+
 import markdown
 import pandas as pd
 from html.parser import HTMLParser
@@ -41,6 +43,20 @@ def parse_token(type_str, value):
     else:
         return value
 
+def parse_table(table_string):
+    table = table_string.split("\n")
+
+    def parse_line(line):
+        line = line.split('|')
+        tokens = line[1:-1]
+        tokens = list(map(lambda x: x[1:-1], tokens))
+        # tokens[1] = parse_token(tokens[-1], tokens[1])
+        return tokens
+
+    header = parse_line(table[0])
+    body = map(parse_line, table[2:])
+    return header, body
+
 class Record:
     def __init__(self, record_path):
         if isinstance(record_path, Recorder):
@@ -66,6 +82,7 @@ class Record:
         self._parse_meta(parser.meta_string)
         self._parse_data(parser.data_string)
 
+
     def _parse_table(self, table_string: str):
         table = table_string.split("\n")
         def parse_line(line):
@@ -79,19 +96,18 @@ class Record:
         return header, body
 
     def _parse_meta(self, meta_string):
-        header, body = self._parse_table(meta_string)
+        header, body = parse_table(meta_string)
         for item in body:
-            self.meta[item[0]] = item[1]
+            self.meta[item[0]] = parse_token(item[2],item[1])
 
     def _parse_data(self, data_string):
-        header, body = self._parse_table(data_string)
-        # df = pd.DataFrame(columns=header)
-        data = np.array(list(body)).astype(float)
-        df = pd.DataFrame(columns=header, data=data)
-        # for item in body:
-        #     data = pd.Series(map(float, item))
-        #     df = df.append(data, ignore_index=True)
-        self.data = df
+        header, body = parse_table(data_string)
+        data = list(body)
+        data = [','.join(seq) for seq in data]
+        data_str = ','.join(header) + '\n' + '\n'.join(data)
+        csv_str = StringIO(data_str)
+        data = pd.read_csv(csv_str)
+        self.data = data
 
     # def get_meta(self):
     #     pass
--- tests/test_procedure.py
+++ tests/test_procedure.py
@@ -33,3 +33,26 @@ def test_create_record(records_dir):
     assert record.meta['epoch'] == 100
     assert record.meta['model'] == 'mlp'
     assert record.data['epoch'][0] == 0
+
+
+def test_hybrid_data_type(records_dir):
+    print(records_dir)
+
+    recorder = Recorder(records_dir=records_dir)
+
+
+    recorder.set_meta({
+        'learning_rate': 0.01,
+        'epoch': 100,
+        'model': 'mlp',
+    })
+    recorder.set_header('data', 'loss', 'acc')
+    recorder.add_data('set1', 0.1, 0.5)
+    recorder.add_data('set1', 0.08, 0.9)
+
+    record = Record(recorder)
+    assert 'learning_rate' in record.meta.keys()
+    assert record.meta['learning_rate'] == 0.01
+    assert record.meta['epoch'] == 100
+    assert record.meta['model'] == 'mlp'
+    assert record.data['data'][0] == 'set1'
